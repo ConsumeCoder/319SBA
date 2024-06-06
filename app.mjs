704233c32e9aa3d01a72cb85f31cdb0bd0d16c45
CoderConsume <epicureantechnologies@gmail.com>
2024-06-05 21:04:07 -0400

    building calendar

--- app.mjs
+++ app.mjs
@@ -7,4 +7,8 @@ const postsRouter = require("./routes/posts.js");
 const error = require("./utilities/error.js");
 const path = require("path");
 
+app.listen(PORT, () => {
+  console.log("app is listening on port", PORT);
+});
+
 app.set("view engine", "ejs");
---
+++ utilities/calConfig.js
@@ -0,0 +1,36 @@
+function calcTable(year) {
+    let arr = new Array(12); for (let m = 0; m < arr.length; m++) {
+         arr[m] = new Array(6);
+        
+    }
+    
+    for (let x = 0; < arr.length; x++) {
+        for (let y = 0; y < arr[x].length; y++) { arr[x][y] = new Array(7);}
+   
+    }
+}
+
+for (let month = 0; month < arr.length; month++) {
+    let startOfWeekDay= new Date(year, month, 0).getDay() + 1;
+
+    let beforeCount = 0;
+    let counter = 1;
+    let startCount = false;
+
+    for (let x = 0; < arr[month].length; x++) {      for (let y = 0; y < arr[month][x].length; y++) {
+        
+        if (beforeCount == startOfWeekDay) {
+            startCount = true;
+            
+        } else { beforeCount++; }
+
+        if (startCount==true) { arr[month][x][y] = counter;
+                    counter++;
+            
+        }
+    }
+    }
+    return arr;
+}
+
+module.exports = calcTable
