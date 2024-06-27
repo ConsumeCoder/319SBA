80200cfaf0cdb660a56e2f3a9ba95a039e78b30f
CoderConsume <epicureantechnologies@gmail.com>
2024-06-27 12:19:44 -0400

    them changes

--- app.mjs
+++ app.mjs
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 3000;
 const usersRouter = require("./routes/users.js");
 const postsRouter = require("./routes/posts.js");
+const notesRouter = require("./routes/notes.js");
 const error = require("./utilities/error.js");
 const path = require("path");
 
@@ -11,10 +12,12 @@ app.listen(PORT, () => {
   console.log("app is listening on port", PORT);
 });
 
-app.set("view-engine", "ejs");
-const path = require("path");
+app.set("view engine", "ejs");
+
 __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "views")));
+
+app.use(users: usersRouter)
 
 app.get("/", (req, res) => {
   const year = req.query.year || 2024;
